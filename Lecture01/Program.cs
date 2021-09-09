463d93faad28f7540b4bd6d8df1ae1b279e7c4dc
isabellaharriesmagnusdottir <imag@itu.dk>
2021-09-09 13:01:01 +0200

    Tested and implemented IsLeapYear

--- Lecture01/Program.cs
+++ Lecture01/Program.cs
@@ -4,9 +4,25 @@ namespace Lecture01
 {
     public class Program
     {
+
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Program.isLeapYear(2000);
+        }
+        public static bool isLeapYear(int year){
+            if(year % 4 == 0)
+            {
+                if(year % 100 == 0)
+                {
+                    if(year % 400 == 0)
+                    {
+                        return true;
+                    }
+                    return false;
+                } 
+                return true;
+            }
+            return false;
         }
     }
 }
